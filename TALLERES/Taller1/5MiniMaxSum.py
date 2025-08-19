a5746ae8c40e1342f012dd07b6287c4ffdec9b20
Juan Pablo Hernández Ortiz <juanpablo.hernandez1@udea.edu.co>
2025-08-19 16:19:59 -0500

    Version 4.3

---
+++ TALLERES/Taller1/5MiniMaxSum.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+from itertools import combinations
+
+def miniMaxSum(arr):
+    lista = []
+    for comb in combinations(arr, 4):
+        lista.append(comb[0] + comb[1] + comb[2] + comb[3])
+    print(min(lista),max(lista))
+valor = list(map(int, input("Escribe los números (mínimo 3): ").split()))
+miniMaxSum(valor)
